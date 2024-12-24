f250f1490a2cd1de95479fdcf2a5bfc14dae0d70
SebDev777 <sebastiangamers17@gmail.com>
2024-12-24 16:50:36 -0500

    Fixed welcome screen

--- src/App.tsx
+++ src/App.tsx
@@ -23,15 +23,23 @@ const WELCOME_STATUS = {
 function App() {
   const [ appState, setAppState ] = useState<AppState>(APP_STATES.LEVEL_SELECTION)
   const [ levelSelected, setLevelSelected ] = useState<number | null>(null)
-  const [showWelcomeScreen, setShowWelcomeScreen] = useState(true);
+  const [showWelcomeScreen, setShowWelcomeScreen] = useState(false);
   const [ welcomeStatus, setWelcomeStatus ] = useState(WELCOME_STATUS.WAITING)
 
   useEffect(() => {
+    const loaded = localStorage.getItem("loaded")
+    if (loaded) {
+      setWelcomeStatus(WELCOME_STATUS.LOADED)
+      return
+    }
+
+    setShowWelcomeScreen(true)
     const handleLoad = () => {
       const timer = setTimeout(() => {
         setWelcomeStatus(WELCOME_STATUS.LOADED)
         const t2 = setTimeout(() => {
           setShowWelcomeScreen(false);
+          localStorage.setItem("loaded", "true")
         }, 2000)
 
         return () => clearTimeout(t2)
