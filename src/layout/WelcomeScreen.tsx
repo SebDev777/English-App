3312328a0125bb088803284852a63d93691216f3
SebDev777 <sebastiangamers17@gmail.com>
2024-12-24 22:21:03 -0500

    Added new things

--- src/layout/WelcomeScreen.tsx
+++ src/layout/WelcomeScreen.tsx
@@ -26,7 +26,7 @@ export default function WelcomeScreen({status} : WelcomeScreenProps) {
                     </div>
                 ) : <h1>Loaded and ready to start!</h1>
             }
-            <span className="author">Made by SebDev, v0.2</span>
+            <span className="author">Made by SebDev, v0.5</span>
         </div>
     );
 }
